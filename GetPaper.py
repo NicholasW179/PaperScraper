dcf566109cdc5503018d92a28b42f470e829dacc
Alex Kyungho Park <kyungho.dev@gmail.com>
2018-11-16 17:22:55 +0900

    #result included

--- GetPaper.py
+++ GetPaper.py
@@ -6,11 +6,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 from openpyxl import load_workbook
 
-driver = webdriver.Chrome('C:/Users/kpark/AppData/Local/Programs/Python/chromedriver.exe')
+#For OSX
+driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+
+#driver = webdriver.Chrome('C:/Users/kpark/AppData/Local/Programs/Python/chromedriver.exe')
 driver.get('http://www.riss.kr/index.do')
 
 #검색어는 지정주어야 함
-def search_riss():
+#학위논문 대상
+def academy_riss():
     handler = driver.window_handles
 
     for i in range(0,4):
@@ -19,14 +23,14 @@ def search_riss():
             query_input = driver.find_element_by_xpath("//input[@id='basicQuery']")
             
         except Exception as e:
-            print('Failure')
+            print('Closing Popups...')
             driver.close()
 
     driver.switch_to_window(handler[0])
 
     # Input of Keywords & Search
     sleep(2)
-    query = "우버"
+    query = "uber"
     query_input.send_keys(query)
     search_btn = driver.find_element_by_xpath("//input[@src='/main/images/sc_btn.gif']")
     search_btn.click()
@@ -39,7 +43,39 @@ def search_riss():
     show_hundred = driver.find_element_by_xpath("//a[contains(text(), '100개씩 출력')]")
     show_hundred.click()
 
-def scrap_data():
+#검색어는 지정주어야 함
+#국내학술지 대상
+def paperwork_riss():
+    handler = driver.window_handles
+
+    for i in range(0,4):
+        try:
+            driver.switch_to_window(handler[i])
+            query_input = driver.find_element_by_xpath("//input[@id='basicQuery']")
+            
+        except Exception as e:
+            print('Closing Popups...')
+            driver.close()
+
+    driver.switch_to_window(handler[0])
+
+    # Input of Keywords & Search
+    sleep(2)
+    query = "우버"
+    query_input.send_keys(query)
+    search_btn = driver.find_element_by_xpath("//input[@src='/main/images/sc_btn.gif']")
+    search_btn.click()
+ 
+    # 학위논문
+    graduate_sec = driver.find_element_by_xpath("//a[contains(text(), '국내학술지논문')]")
+    graduate_sec.click()
+
+    #100개씩 보여주기
+    show_hundred = driver.find_element_by_xpath("//a[contains(text(), '100개씩 출력')]")
+    show_hundred.click()
+
+
+def scrap_academy():
     title_candidates = driver.find_elements_by_xpath("//p[@class='txt']") #전체 돌아가는 숫자
     val = 0 #저자, 소속을 위한 임시 변수
 
@@ -88,8 +124,67 @@ def scrap_data():
         except Exception as e:
             print("Not on its format2") 
 
+        # Put infos on Excel
+        workbook = load_workbook("/Users/Kyungho/Desktop/PaperScraper/temp.xlsx")
+        worksheet = workbook.active
         
+        title_idx = 1
+        author_idx = 2
+        association_idx = 3
+        year_idx = 4
 
+        row_idx = element + 1
+
+        worksheet.cell(row=row_idx, column=title_idx, value=paper_title)
+        worksheet.cell(row=row_idx, column=author_idx, value=paper_author)
+        worksheet.cell(row=row_idx, column=association_idx, value=paper_association)    
+        worksheet.cell(row=row_idx, column=year_idx, value=paper_year)
+        workbook.save("/Users/Kyungho/Desktop/PaperScraper/temp.xlsx")
+
+
+def scrap_paperwork():
+    
+    title_candidates = driver.find_elements_by_xpath("//p[@class='txt']") #전체 돌아가는 숫자를 정의하기 위해
+    url_checkpoint = driver.current_url #검색결과 화면을 체크
+     
+    for element in range(0,len(title_candidates)):
+
+        print(element)
+        sleep(3)
+        graduate_paper_list = driver.find_elements_by_xpath("//p[@class='txt']/a")
+        graduate_paper_list[element].click() #for문 돌려서 바꿔주어야 함
+
+        #Title
+        title_temp = driver.find_element_by_xpath("//div[@class='vTop02']")
+        paper_title = title_temp.text
+
+        #Sub infos - make as grouped body
+        info_temp = driver.find_elements_by_xpath("//p[@class='w56']")
+
+        #Author
+        paper_author = info_temp[1].text
+
+        #Association
+        paper_association = info_temp[4].text
+
+        #Year
+        list_temp = driver.find_elements_by_xpath("//ul[@class='report']/li/p")
+        paper_year = list_temp[9].text
+
+        #URL
+        url_temp = driver.find_element_by_xpath("//div[@class='vTop02']/p[@class='copy']/a")
+        paper_url = url_temp.text
+
+        print(paper_title)
+        print(paper_author)
+        print(paper_association)
+        print(paper_year)
+        print(paper_url)
+
+        print(element)
+        driver.get(url_checkpoint)
+        sleep(1)
+    
 
 def search_list_by_clicking():
     #전체 페이퍼 항목들을 보여줌
@@ -128,15 +223,27 @@ def search_list_by_clicking():
         print(paper_year)
         print(paper_url)
 
-        driver.execute_script("window.history.go(-1)") # Backspacing is not working
-        driver.refresh()
-        sleep(1)    
+def test_excel():
+    workbook = load_workbook("/Users/Kyungho/Desktop/PaperScraper/temp.xlsx")
+    worksheet = workbook.active
+    
+    row_idx = element + 1
+    title_idx = 1
+    author_idx = 2
+    association_idx = 3
+    year_idx = 4
+
+    worksheet.cell(row=row_idx, column=title_idx, value='제목')
+    worksheet.cell(row=row_idx, column=author_idx, value='저자')
+    worksheet.cell(row=row_idx, column=association_idx, value='소속')
+    worksheet.cell(row=row_idx, column=year_idx, value='연도')
+    workbook.save("/Users/Kyungho/Desktop/PaperScraper/temp.xlsx")
+
+    row_idx = row_idx + 1
 
 
+#academy_riss()
+paperwork_riss()
 
-
-
-
-
-search_riss()
-scrap_data()
+#scrap_academy()
+scrap_paperwork()
